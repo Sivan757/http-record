f069f2233ad25fe58ff63fc1c8d3266ac014af63
sivan <zifanuu@gmail.com>
2022-01-18 00:27:41 +0800

    chore(workflows):调整native构建参数

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-native-image --no-fallback --enable-all-security-services --report-unsupported-elements-at-runtime \
+native-image --no-fallback --report-unsupported-elements-at-runtime \
 --install-exit-handlers --allow-incomplete-classpath --initialize-at-build-time=io.ktor,kotlinx,kotlin,org\
---target=debian-arm64 -H:+ReportUnsupportedElementsAtRuntime -H:+ReportExceptionStackTraces \
--H:ReflectionConfigurationFiles=./reflection.json -cp ./build/libs/http-record-0.0.1-all.jar \
--H:Class=cn.kapukapu.ApplicationKt -H:Name=http-record
+-H:+ReportUnsupportedElementsAtRuntime -H:+ReportExceptionStackTraces \
+-H:ReflectionConfigurationFiles=reflection.json -jar ./build/libs/http-record-1.1.0-all.jar
