7d15bad1b29b887f48b41ee154b03a3e035e551f
sivan <zifanuu@gmail.com>
2022-01-19 22:23:16 +0800

    chore(workflow):修复 native 构建

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 native-image --no-fallback --report-unsupported-elements-at-runtime \
---install-exit-handlers --allow-incomplete-classpath --initialize-at-build-time=io.ktor,kotlinx,kotlin,org\
+--install-exit-handlers --allow-incomplete-classpath --initialize-at-build-time=io.ktor,kotlinx,kotlin,ch.qos.logback \
 -H:+ReportUnsupportedElementsAtRuntime -H:+ReportExceptionStackTraces \
 -H:ReflectionConfigurationFiles=reflection.json -jar ./build/libs/http-record-1.1.0-all.jar
