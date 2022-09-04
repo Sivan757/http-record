fa2889de4d5e446dcd2f5b02ff19c554b6654b9f
sivan <zifanuu@gmail.com>
2022-09-04 23:29:41 +0800

    build(native): 修正native-image脚本，以便docker多阶段构建

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 native-image --no-fallback --report-unsupported-elements-at-runtime \
---install-exit-handlers --allow-incomplete-classpath --initialize-at-build-time=io.ktor,kotlinx,kotlin,ch.qos.logback \
+--install-exit-handlers --initialize-at-build-time=io.ktor,kotlinx,kotlin,ch.qos.logback \
 -H:+ReportUnsupportedElementsAtRuntime -H:+ReportExceptionStackTraces \
 -H:ReflectionConfigurationFiles=reflection.json -jar ./build/libs/http-record-1.1.0-all.jar
+
